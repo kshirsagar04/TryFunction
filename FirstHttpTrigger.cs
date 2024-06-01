1a200b74cbae478748e764095860401a17afa4b9
Rajashri <Kshirsagar.rajashri@hotmail.com>
2024-06-01 19:56:46 +0530

    first commit

---
+++ FirstHttpTrigger.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class FirstHttpTrigger
+    {
+        private readonly ILogger<FirstHttpTrigger> _logger;
+
+        public FirstHttpTrigger(ILogger<FirstHttpTrigger> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("FirstHttpTrigger")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
